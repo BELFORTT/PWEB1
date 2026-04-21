1eda63381812b47301da80635fa2176b5f0516e8
BELFORTT <victorbelfort10@hotmail.com>
2026-04-21 11:19:47 -0300

    feat: organizando bagunca de pastas

---
+++ angular/app-teste-angular/src/app/app.ts
@@ -0,0 +1,14 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import {Footer} from './footer/footer'
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, Footer],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  protected readonly title = signal('app-teste-angular');
+  public disciplina: string = "Programação para Web I";
+}
